c35469c2b5506ccf0c68b1eb50d451ab51f216c3
Victor Luevano <victorluevmon@gmail.com>
2024-05-29 06:53:25 -0600

    Se inicializó el proyecto

---
+++ src/test/java/java/uv/mx/Recetario/RecetarioApplicationTests.java
@@ -0,0 +1,13 @@
+package java.uv.mx.Recetario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
