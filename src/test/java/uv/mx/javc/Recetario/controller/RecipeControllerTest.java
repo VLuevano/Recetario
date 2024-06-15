c31b7fc3a58b64702fd34bff566f6c40b88cc86c
Victor <victorluevmon@gmail.com>
2024-06-15 10:29:01 -0600

    Se actualizaron los scripts de pruebas

--- src/test/java/uv/mx/javc/Recetario/controller/RecipeControllerTest.java
+++ src/test/java/uv/mx/javc/Recetario/controller/RecipeControllerTest.java
@@ -29,52 +29,42 @@ public class RecipeControllerTest {
 
     @Test
     void testGetAll() {
-        // Simulamos el comportamiento del servicio
         List<RecipeDTO> recipes = Arrays.asList(
-            new RecipeDTO("1", "Tarta de Manzana", "Deliciosa tarta de manzana", Arrays.asList("manzanas", "harina"), Arrays.asList("Paso 1", "Paso 2")),
-            new RecipeDTO("2", "Ensalada César", "Clásica ensalada César", Arrays.asList("lechuga", "pollo"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"))
+            new RecipeDTO("1", "Receta 1", "Descripción", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2")),
+            new RecipeDTO("2", "Receta 2", "Descripción", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"))
         );
         when(recipeService.getAll()).thenReturn(recipes);
 
-        // Llamamos al método del controlador y verificamos el resultado
         List<RecipeDTO> result = recipeController.getAll();
         assertEquals(2, result.size());
     }
 
     @Test
     void testSave() {
-        // Datos de prueba para la receta
-        CreateRecipeDTO data = new CreateRecipeDTO("Paella", "Deliciosa paella", Arrays.asList("arroz", "pollo"), Arrays.asList("Paso 1", "Paso 2"));
+        CreateRecipeDTO data = new CreateRecipeDTO("Receta 1", "Descripción", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2"));
 
-        // Simulamos el comportamiento del servicio
-        RecipeDTO savedRecipe = new RecipeDTO("1", "Paella", "Deliciosa paella", Arrays.asList("arroz", "pollo"), Arrays.asList("Paso 1", "Paso 2"));
+        RecipeDTO savedRecipe = new RecipeDTO("1", "Receta 1", " Descripción", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2"));
         when(recipeService.save(any(CreateRecipeDTO.class))).thenReturn(savedRecipe);
 
-        // Llamamos al método del controlador y verificamos el resultado
         RecipeDTO result = recipeController.save(data);
-        assertEquals("Paella", result.getTitle());
+        assertEquals("Receta 1", result.getTitle());
     }
 
     @Test
     void testUpdate() {
-        // Datos de prueba para la receta
-        CreateRecipeDTO data = new CreateRecipeDTO("Paella Valenciana", "Receta actualizada de paella", Arrays.asList("arroz", "pollo"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"));
+        CreateRecipeDTO data = new CreateRecipeDTO("Receta 1", "Descripción", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"));
 
-        // Simulamos el comportamiento del servicio
-        RecipeDTO updatedRecipe = new RecipeDTO("1", "Paella Valenciana", "Receta actualizada de paella", Arrays.asList("arroz", "pollo"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"));
+        RecipeDTO updatedRecipe = new RecipeDTO("1", "Receta 1", "Descripción nueva", Arrays.asList("Ingrediente 1", "Ingrediente 2"), Arrays.asList("Paso 1", "Paso 2", "Paso 3"));
         when(recipeService.update(anyString(), any(CreateRecipeDTO.class))).thenReturn(Optional.of(updatedRecipe));
 
-        // Llamamos al método del controlador y verificamos el resultado
         Optional<RecipeDTO> result = recipeController.update("1", data);
-        assertEquals("Paella Valenciana", result.get().getTitle());
+        assertEquals("Receta 1", result.get().getTitle());
     }
 
     @Test
     void testDelete() {
-        // Simulamos el comportamiento del servicio
         when(recipeService.delete(anyString())).thenReturn(true);
 
-        // Llamamos al método del controlador y verificamos el resultado
         boolean result = recipeController.delete("1");
         assertEquals(true, result);
     }
